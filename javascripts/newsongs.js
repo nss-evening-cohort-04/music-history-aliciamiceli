767ad9008f3518392b9a6135fcc2b9b322f0686e
Alicia Miceli <aliciamiceli@Alicias-MacBook-Pro.local>
2016-10-10 18:48:04 -0500

    error free, lint free, and working

--- javascripts/newsongs.js
+++ javascripts/newsongs.js
@@ -5,6 +5,7 @@
 // request.send();
 // request.addEventListener('load', runThisFunctionWhenDomIsLoaded);
 // var currentSong;
+  var parseSongsToDom = "";
 
 $(document).ready(function (){
   $.ajax({ url: 'json/songs.json',
@@ -18,28 +19,21 @@ $(document).ready(function (){
   for (var i = 0; i < 3; i++) {
    let currentSong = data1.songs[i];
     console.log("this is inside the for loop", data1.songs[i]);
-  let parseSongsToDom = "";
   console.log("parseSongsToDom", parseSongsToDom);
 
-    parseSongsToDom = `<div id=data${i}>${currentSong.title} `;
+    parseSongsToDom += `<div id=data${i}>${currentSong.title} `;
       parseSongsToDom += `by ${currentSong.artist} `;
       parseSongsToDom += `on the album ${currentSong.album}`;
       parseSongsToDom += `<button id=button${i}>Delete</button>`;
     parseSongsToDom += `</div>`;
 
-  }
-    }
-  })
-
   $('#songsList').html(parseSongsToDom);
-
-})
-
   $("#button0").click(buttonFunction0);
   $("#button1").click(buttonFunction1);
   $("#button2").click(buttonFunction2);
 
   function buttonFunction0() {
+
     $('#data0').remove();
   }
 
@@ -50,6 +44,14 @@ $(document).ready(function (){
   function buttonFunction2(){
     $('#data2').remove();
   }
+    }
+  }
+});
+  });
+
+
+
+
 
 $('#more').click(moreFunction);
 
@@ -63,9 +65,9 @@ let parseMoreSongsToDom = "";
 
 for (let j=0; j < 3; j++) {
   console.log("data.songs[j]", data.songs[j]);
- let currentSongSecond = data.songs[j]
+ let currentSongSecond = data.songs[j];
 
-    parseMoreSongsToDom += `<div id=data${i}>${currentSongSecond.title} `;
+    parseMoreSongsToDom += `<div id=data${j+3}>${currentSongSecond.title} `;
       parseMoreSongsToDom += `by ${currentSongSecond.artist} `;
       parseMoreSongsToDom += `on the album ${currentSongSecond.album}`;
     parseMoreSongsToDom += `</div>`;
@@ -74,7 +76,7 @@ console.log("parseMoreSongsToDom", parseMoreSongsToDom);
 
 }
 
-$('#songsList').append(parseMoreSongsToDom)
+$('#songsList').append(parseMoreSongsToDom);
 
 
 
